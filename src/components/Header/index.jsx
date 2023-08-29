ca396e2c51ec6aa140a7a2e1bed733192d42c7a6
Josenilsonfariasx <fariaslwork@gmail.com>
2023-08-29 18:30:43 -0300

    refactor :move state of place

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,12 @@
 import cartIcon from "../../assets/cartIcon.png"
 import styles from "./style.module.scss"
 
-export const Header = () =>{
+export const Header = ({setModalVisible}) =>{
     
     return (
         <header className={styles.headerStyle}>
             <span className="title m">FASHIONSTORE</span>
-            <button>
+            <button onClick={(()=> setModalVisible(true))}>
                 <img src={cartIcon} alt="Shopping cart button" />
             </button>
         </header>
