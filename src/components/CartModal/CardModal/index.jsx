88f3fffa502fb8838a4b7f81a02034bcfcaa8136
Josenilsonfariasx <fariaslwork@gmail.com>
2023-08-29 06:25:03 -0300

    feat: adding function remove item from cart

--- src/components/CartModal/CardModal/index.jsx
+++ src/components/CartModal/CardModal/index.jsx
@@ -1,20 +1,36 @@
 import deleteSvg from "../../../assets/deleteItem.svg";
 import style from "./style.module.scss";
-const CardModal = () => {
+const CardModal = ({ product, setItemCard, productsListToCard, index }) => {
+    const price = Number(product.price).toLocaleString("pt-BR", {
+        minimumFractionDigits: 2,
+    });
+    const removeItem = (idToRemove, indexToRemove) => {
+        const newListProduct = productsListToCard.filter(
+            (product, index) =>
+                !(product.id === idToRemove && index === indexToRemove)
+        );
+        setItemCard(newListProduct);
+    };
     return (
         <>
-            <section className={style.sc}>
-                <div className={style.card}>
-                    <div>
-                        <img src="" alt="" />
+                <li className={style.sc}>
+                    <div className={style.card}>
+                        <div>
+                            <img src={product.image} alt={product.name} />
+                            {/* <span className={style.nItem}>
+                            </span> */}
+                        </div>
+                        <div className={style.infoCard}>
+                            <span className="title sm">{product.name}</span>
+                            <span className="price sm">{price}</span>
+                        </div>
                     </div>
-                    <div className={style.infoCard}>
-                        <span className="title sm">Blazer Branco Elegante</span>
-                        <span className="price sm">R$ 320,00 </span>
-                    </div>
-                </div>
-                <img src={deleteSvg} alt="" />
-            </section>
+                    <img
+                        src={deleteSvg}
+                        alt="deletar"
+                        onClick={() => removeItem(product.id, index)}
+                    />
+                </li>
         </>
     );
 };
