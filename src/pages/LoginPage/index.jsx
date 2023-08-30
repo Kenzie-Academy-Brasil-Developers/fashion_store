5321fe96fef4b7907cbb6ce9c817628f661c368e
Josenilsonfariasx <fariaslwork@gmail.com>
2023-08-30 02:57:55 -0300

    feat: adding loginPage components

---
+++ src/pages/LoginPage/index.jsx
@@ -0,0 +1,48 @@
+import DefaultPageAdmin from "../../components/DeafaultPageAdmin";
+import banner from "../../assets/banner.png";
+
+import style from "./style.module.scss";
+import { Input } from "../../components/form/Input";
+import { InputPassword } from "../../components/form/InputPassword";
+import { useContext } from "react";
+import { UserContext } from "../../providers/userContext";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+
+const LoginPage = () => {
+    const navigate = useNavigate()
+    const { register, handleSubmit } = useForm();
+    const { userLogin } = useContext(UserContext);
+
+    const submit = (formData) =>{
+        userLogin(formData)
+    }
+
+    return (
+        <>
+            <DefaultPageAdmin>
+                <section className={style.login}>
+                    <div className={style.banner}>
+                        <img src={banner} alt="" />
+                    </div>
+                    <section className={style.form}>
+                        <span className="title l">ENTRAR</span>
+                        <div>
+                            <form onSubmit={handleSubmit(submit)}>
+                                <Input type="email" placeholder="E-MAIL" {...register('email')} />
+                                <InputPassword placeholder="SENHA" {...register('password')} />
+                                <div className={style.btns}>
+                                    <button type="submit" className="button black">ACESSAR</button>
+                                    <button type="button" className="button white" onClick={()=> navigate("/register")}>CADASTRAR-SE</button>
+                                </div>
+                            </form>
+                        </div>
+                    </section>
+                </section>
+            </DefaultPageAdmin>
+        </>
+    );
+};
+
+export default LoginPage;
