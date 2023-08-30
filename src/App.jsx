272ae119e12640e36ac86dc1c722e8c959b4181e
Josenilsonfariasx <fariaslwork@gmail.com>
2023-08-30 02:59:05 -0300

    feat: adding toastfy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./styles/index.scss";
 import RoutesMain from "./routes/routesMain";
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 const App = () => {
     return (
         <>
             <RoutesMain />
+            <ToastContainer
+        position="top-center"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        />
         </>
     );
 };
