acc367c4eb37f27735c1dc817e5a8c1bc45ed866
Karengrabarz <karengrabarz@gmail.com>
2023-08-30 13:06:50 -0300

    feat: responsividade RegisterPage
    fix: estilizçao p
    
    fix:toast  erro toast login

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,23 +1,30 @@
-import registerImg from '../../assets/registerImage.png'
-import backArrow from '../../assets/backArrow.png'
-import { RegisterForm } from '../../components/form/RegisterForm'
-import styles from './style.module.scss'
+import registerImg from "../../assets/registerImage.png";
+import backArrow from "../../assets/backArrow.png";
+import { RegisterForm } from "../../components/form/RegisterForm";
+import styles from "./style.module.scss";
+import DefaultPageAdmin from "../../components/DeafaultPageAdmin";
+import { useNavigate } from "react-router";
 
-export const RegisterPage = () =>{
-    return(
-        <main>
-            <div className={styles.containerBox}>
-                <img src={registerImg} alt='register image'/>
-                <div className={styles.flexBox}>
-                    <button className={styles.buttonBack}>
-                        <img src={backArrow} alt='back Arrow'/>
-                        <p className='paragraph m'>VOLTAR</p>
-                    </button>
-                    <h1 className='title xl mobile'>CADASTRAR-SE</h1>
-                    <p className='paragraph'>Seja bem vindo, administrador!</p>
-                    <RegisterForm/>
-                </div>
-            </div>
-        </main>
-    )
-}
+export const RegisterPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <DefaultPageAdmin>
+      <div className={styles.containerBox}>
+        <img src={registerImg} alt="register image" />
+        <div className={styles.flexBox}>
+          <button
+            onClick={() => navigate("/login")}
+            className={styles.buttonBack}
+          >
+            <img src={backArrow} alt="back Arrow" />
+            <p className="paragraph m">VOLTAR</p>
+          </button>
+          <h1 className="title xl mobile">CADASTRAR-SE</h1>
+          <p className="paragraph">Seja bem vindo, administrador!</p>
+          <RegisterForm />
+        </div>
+      </div>
+    </DefaultPageAdmin>
+  );
+};
